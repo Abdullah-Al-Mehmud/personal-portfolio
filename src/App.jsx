c0e57774027f1a5ba743e33f1b14bc20a5af85e2
mehmud <timehmud121@gmail.com>
2023-12-10 21:55:03 +0600

    added navbar

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import Header from "./Components/Header/Header";
+import Hero from "./Components/Hero/Hero";
+
+function App() {
+  return (
+    <>
+      <Header></Header>
+      <Hero></Hero>
+    </>
+  );
+}
+
+export default App;
